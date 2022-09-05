681e367311f2ae0b29c376b96a94c9c94d5b71dc
David Rakosi <61084415+davidrakosi@users.noreply.github.com>
2022-09-04 17:30:40 -0700

    ✅ Fix

--- app/utils/moonBirdsContract.js
+++
@@ -1,8 +0,0 @@
-import { contractABI, contractAddress } from './constants'
-
-const moonBirdsContract = web3Instance => {
-  console.log(contractAddress)
-  return new web3Instance.eth.Contract(contractABI, contractAddress)
-}
-
-export default moonBirdsContract
